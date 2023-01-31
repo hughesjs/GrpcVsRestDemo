851869189bbc49f05c9032ecbbf63fedd2439548
James Hughes <james.hughes@spektrix.com>
2023-01-31 22:07:39 +0000

    Switched back to nuget package

--- SerialisationBenchmark/Program.cs
+++ SerialisationBenchmark/Program.cs
@@ -1,19 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-using System.Text;
+﻿using System.Text;
 using AutoFixture;
 using Benchmark;
 using Google.Protobuf;
-using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
-using Google.Protobuf.Collections;
 using ProtobufDemo.ProtoModels;
 using JsonSerializer = System.Text.Json.JsonSerializer;
 
 // Run serialisation benchmark
-Summary summary = BenchmarkRunner.Run<JsonVsProtoBenchmark>();
-
+BenchmarkRunner.Run<JsonVsProtoBenchmark>();
 
 //Get size difference
 Fixture fixture = new();
@@ -25,12 +19,10 @@ string json = JsonSerializer.Serialize(book);
 byte[] encodedBytes = Encoding.UTF8.GetBytes(json);
 Console.WriteLine($"JSON Payload Size: {encodedBytes.Length}B");
 
-
 using MemoryStream memStream = new();
 CodedOutputStream codedOutputStream = new(memStream);
 
 book.WriteTo(codedOutputStream);
-
 codedOutputStream.Flush();
 
 Console.WriteLine($"Protobuf Payload Size: {memStream.ToArray().Length}B");
