1b1e7a04a0ad082e99b0964486fd7f5eb19292bc
James Hughes <james.hughes@spektrix.com>
2023-01-31 23:11:12 +0000

    Working basic dad joke service

---
+++ GRpcDemoProject/DadJokeService.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+using ProtobufDemo.ProtoServices;
+
+namespace GRpcDemoProject;
+
+public class DadJokeService: ProtobufDemo.ProtoServices.DadJokeService.DadJokeServiceBase
+{
+    public override Task<DadJoke> GetDadJoke(DadJokeRequest request, ServerCallContext context)
+    {
+        string joke = request.Category switch
+        {
+            DadJokeRequest.Types.Category.Puns => "Why was the math book sad? Because it had too many problems.",
+            DadJokeRequest.Types.Category.OneLiners => "I used to play piano by ear, but now I use my hands.",
+            DadJokeRequest.Types.Category.PlayOnWords => "I told my wife she was drawing her eyebrows too high. She looked surprised.",
+            DadJokeRequest.Types.Category.Anticlimactic => "I'm reading a book on anti-gravity. It's impossible to put down.",
+            DadJokeRequest.Types.Category.Irony => "I used to be a software developer, but now I'm just a debug specialist.",
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        DadJoke response = new()
+        {
+            Joke = joke
+        };
+
+        return Task.FromResult(response);
+    }
+}
---
+++ GRpcDemoProject/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GRpcDemoProject;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.json", true);
+builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+
+WebApplication app = builder.Build();
+app.MapGrpcService<DadJokeService>();
+app.MapGrpcReflectionService();
+
+app.Run();
